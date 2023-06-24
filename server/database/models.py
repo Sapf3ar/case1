3165139d1450783cb6e6ee1aa2952c4bb8d0a346
Ivan <buran05@icloud.com>
2023-06-24 08:09:51 +0300

    Initial commit

---
+++ server/database/models.py
@@ -0,0 +1,42 @@
+import uuid
+
+from peewee import Model, AutoField, CharField, IntegerField, ForeignKeyField, TextField, UUIDField, BooleanField
+from server.database.base import db
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Transaction(BaseModel):
+    id = UUIDField(primary_key=True, default=uuid.uuid4)
+
+
+class Report(BaseModel):
+    id = AutoField()
+    transaction = ForeignKeyField(Transaction, backref='reports')
+    reference = CharField(null=True)
+    files_count = IntegerField(null=True)
+    errors_count = IntegerField(null=True)
+    match_count = IntegerField(null=True)
+
+
+class File(BaseModel):
+    id = AutoField()
+    report = ForeignKeyField(Report, backref='files')
+    is_checked = BooleanField(null=True, default=False)
+    filename = CharField(null=True)
+
+
+class Error(BaseModel):
+    id = AutoField()
+    file = ForeignKeyField(File, backref='errors')
+    description = TextField(null=True)
+    subject = CharField(null=True)
+    match = IntegerField(null=True)
+    page = IntegerField(null=True)
+    line = IntegerField(null=True)
+
+
+db.create_tables([Transaction, Report, File, Error])
---
+++ server/helpers/parsers.py
@@ -0,0 +1,40 @@
+from server.models import File, Error
+
+
+def db_error_to_error(db_error) -> Error:
+    error = Error(
+        id=db_error.id,
+        description=db_error.description,
+        subject=db_error.subject,
+        match=db_error.match,
+        page=db_error.match,
+        line=db_error.line
+    )
+    return error
+
+
+def db_errors_to_errors(db_errors) -> list:
+    result = []
+    for db_error in db_errors:
+        result.append(db_error_to_error(db_error))
+    return result
+
+
+def db_file_to_file(db_file) -> File:
+    file = File(
+        id=db_file.id,
+        filename=db_file.filename,
+        is_checked=db_file.is_checked,
+        errors=db_errors_to_errors(db_file.errors)
+    )
+
+    return file
+
+
+def db_files_to_files(db_files) -> list:
+    result = []
+
+    for db_file in db_files:
+        result.append(db_file_to_file(db_file))
+
+    return result
