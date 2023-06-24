162dbddccff2461ff99721f030cdfde992971102
ShmatEugene <shmat.evg@yandex.ru>
2023-06-24 11:24:44 +0300

    front: добавить описание ошибки

--- frontend/src/components/FileErrors.tsx
+++ frontend/src/components/FileErrors.tsx
@@ -1,4 +1,6 @@
+import { Descriptions } from 'antd';
 import React from 'react';
+import ReactDiffViewer from 'react-diff-viewer';
 // import { Error } from '../api/models/reports-response.ts';
 
 interface Error {
@@ -19,7 +21,34 @@ const FileErrors = ({ errors, refference }: Props) => {
     return (
         <div>
             {errors.map((error) => {
-                return <div key={error.id}>{error.description}</div>;
+                return (
+                    <Descriptions
+                        className='file-errors__description'
+                        key={error.id}
+                        title='Ошибка в файле'
+                        bordered
+                    >
+                        <Descriptions.Item label='Тип ошибки'>
+                            {error.description}
+                        </Descriptions.Item>
+                        <Descriptions.Item label='Ожидаемое наименование ОКС'>
+                            {refference}
+                        </Descriptions.Item>
+                        <Descriptions.Item label='Фактическое наименование ОКС'>
+                            {error.subject}
+                        </Descriptions.Item>
+                        <Descriptions.Item label='Разница' span={10}>
+                            <ReactDiffViewer
+                                oldValue={error.subject}
+                                newValue={refference}
+                                splitView={true}
+                            />
+                        </Descriptions.Item>
+                        <Descriptions.Item label='Место в файле'>
+                            страница {error.page}, строка {error.line}
+                        </Descriptions.Item>
+                    </Descriptions>
+                );
             })}
         </div>
     );
